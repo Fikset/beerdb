ce6eccf33c761f7a8603150dac2f6f0681be7a94
yuriy.bezgin <y.bezgin@pbd-team.ru>
2023-09-07 11:56:30 +0100

    add prometheus metrics

--- src/app.py
+++ src/app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request
+from prometheus_client import generate_latest, Summary
 
 app = Flask(__name__)
 
+
+# Create a metric to track time spent and requests made.
+REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+
+
+# Decorate function with metric.
 @app.route("/")
 def main():
     return '''
@@ -14,6 +21,17 @@ def main():
      '''
 
 @app.route("/echo_user_input", methods=["POST"])
+@REQUEST_TIME.time()
 def echo_input():
     input_text = request.form.get("user_input", "")
     return "You entered: " + input_text
+
+@app.route("/health", methods=["GET"])
+@REQUEST_TIME.time()
+def echo_ok():
+    return "OK"
+
+@app.route("/metrics", methods=["GET"])
+@REQUEST_TIME.time()
+def echo_requests():
+    return generate_latest()
